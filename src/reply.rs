e8ff377a3581489ff7f4aa7fbcd724aac03ad2d5
vicky5124 <vickyf5124@gmail.com>
2021-09-01 00:30:42 +0200

    Add a component interactions example with message edits. 	modified:   Cargo.toml 	new file:   examples/component_interactions.rs 	renamed:    examples/main.rs -> examples/framework_usage.rs 	modified:   src/reply.rs

--- src/reply.rs
+++ src/reply.rs
@@ -11,8 +11,8 @@ pub struct CreateReply<'a> {
 
 impl<'a> CreateReply<'a> {
     /// Set the content of the message.
-    pub fn content(&mut self, content: String) -> &mut Self {
-        self.content = Some(content);
+    pub fn content(&mut self, content: impl ToString) -> &mut Self {
+        self.content = Some(content.to_string());
         self
     }
 
@@ -71,7 +71,7 @@ pub async fn send_reply<U, E>(
 
 pub async fn say_reply<U, E>(
     ctx: crate::Context<'_, U, E>,
-    text: String,
+    text: impl ToString,
 ) -> Result<(), serenity::Error> {
     send_reply(ctx, |m| m.content(text)).await
 }
