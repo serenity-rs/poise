843c93eeb681aa8935024c7dff18a8c2920c309b
kangalioo <jannik.a.schaper@web.de>
2021-12-21 22:36:06 +0100

    Split big function into two medium sized functions

--- macros/src/command/prefix.rs
+++ macros/src/command/prefix.rs
@@ -11,47 +11,11 @@ pub fn generate_prefix_command_spec(
         None
     };
 
-    let param_specs =
-        inv.parameters
-            .iter()
-            .map(|p| {
-                enum Modifier {
-                    None,
-                    Lazy,
-                    Flag,
-                    Rest,
-                }
-
-                let modifier =
-                    match (p.more.lazy, p.more.rest, p.more.flag) {
-                        (false, false, false) => Modifier::None,
-                        (true, false, false) => Modifier::Lazy,
-                        (false, true, false) => Modifier::Rest,
-                        (false, false, true) => Modifier::Flag,
-                        _ => return Err(syn::Error::new(
-                            p.span,
-                            "modifiers like #[lazy] or #[rest] currently cannot be used together",
-                        )
-                        .into()),
-                    };
-
-                let type_ = &p.type_;
-                Ok(match modifier {
-                    Modifier::Flag => {
-                        if p.type_ != syn::parse_quote! { bool } {
-                            return Err(
-                                syn::Error::new(p.type_.span(), "Must use bool for flags").into()
-                            );
-                        }
-                        let literal = proc_macro2::Literal::string(&p.name.to_string());
-                        quote::quote! { #[flag] (#literal) }
-                    }
-                    Modifier::Lazy => quote::quote! { #[lazy] (#type_) },
-                    Modifier::Rest => quote::quote! { #[rest] (#type_) },
-                    Modifier::None => quote::quote! { (#type_) },
-                })
-            })
-            .collect::<Result<Vec<_>, darling::Error>>()?;
+    let param_specs = inv
+        .parameters
+        .iter()
+        .map(quote_parameter)
+        .collect::<Result<Vec<_>, darling::Error>>()?;
 
     let command_name = &inv.command_name;
     let track_edits = inv.more.track_edits;
@@ -66,13 +30,60 @@ pub fn generate_prefix_command_spec(
                     ctx.discord, ctx.msg, args =>
                     #( #param_specs, )*
                     #wildcard_arg
-                ).await?;
-                inner(ctx.into(), #( #param_names, )* ).await
+                ).await.map_err(|error| poise::FrameworkError::ArgumentParse {
+                    ctx: ctx.into(),
+                    error: error.0
+                })?;
+
+                inner(ctx.into(), #( #param_names, )* )
+                    .await
+                    .map_err(|error| poise::FrameworkError::Command {
+                        error,
+                        location: poise::CommandErrorLocation::Body,
+                        ctx: ctx.into(),
+                    })
             }),
             id: std::sync::Arc::clone(&command_id),
             track_edits: #track_edits,
             broadcast_typing: #broadcast_typing,
             aliases: &[ #( #aliases, )* ],
         }
+    })
+}
+
+fn quote_parameter(
+    p: &super::CommandParameter,
+) -> Result<proc_macro2::TokenStream, darling::Error> {
+    enum Modifier {
+        None,
+        Lazy,
+        Flag,
+        Rest,
+    }
+    let modifier = match (p.more.lazy, p.more.rest, p.more.flag) {
+        (false, false, false) => Modifier::None,
+        (true, false, false) => Modifier::Lazy,
+        (false, true, false) => Modifier::Rest,
+        (false, false, true) => Modifier::Flag,
+        _ => {
+            return Err(syn::Error::new(
+                p.span,
+                "modifiers like #[lazy] or #[rest] currently cannot be used together",
+            )
+            .into())
+        }
+    };
+    let type_ = &p.type_;
+    Ok(match modifier {
+        Modifier::Flag => {
+            if p.type_ != syn::parse_quote! { bool } {
+                return Err(syn::Error::new(p.type_.span(), "Must use bool for flags").into());
+            }
+            let literal = proc_macro2::Literal::string(&p.name.to_string());
+            quote::quote! { #[flag] (#literal) }
+        }
+        Modifier::Lazy => quote::quote! { #[lazy] (#type_) },
+        Modifier::Rest => quote::quote! { #[rest] (#type_) },
+        Modifier::None => quote::quote! { (#type_) },
     })
 }
