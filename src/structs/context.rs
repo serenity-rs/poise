5f589a70a25ffcee5a86df5ff5f8ab2b5c6db8db
alakhpc <36898190+alakhpc@users.noreply.github.com>
2022-10-23 13:27:34 -0400

    impl AsRef<Http>, AsRef<Cache> for Context

--- src/structs/context.rs
+++ src/structs/context.rs
@@ -433,3 +433,15 @@ impl<'a, U, E> From<Context<'a, U, E>> for PartialContext<'a, U, E> {
         }
     }
 }
+
+impl<'a, U, E> AsRef<serenity::Http> for Context<'a, U, E> {
+    fn as_ref(&self) -> &serenity::Http {
+        &self.discord().http
+    }
+}
+
+impl<'a, U, E> AsRef<serenity::Cache> for Context<'a, U, E> {
+    fn as_ref(&self) -> &serenity::Cache {
+        &self.discord().cache
+    }
+}
