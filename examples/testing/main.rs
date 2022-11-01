cceac770b616da33444da022e97cc0631580b773
kangalioo <jannik.a.schaper@web.de>
2022-11-02 00:57:03 +0100

    Do permission checks for all parent commands too

--- examples/testing/main.rs
+++ examples/testing/main.rs
@@ -5,16 +5,39 @@ type Context<'a> = poise::Context<'a, Data, Error>;
 // User data, which is stored and accessible in all command invocations
 struct Data {}
 
-#[poise::command(slash_command, prefix_command)]
+async fn child2_check(_ctx: Context<'_>) -> Result<bool, Error> {
+    println!("Child2 check executed!");
+    Ok(true)
+}
+async fn child1_check(_ctx: Context<'_>) -> Result<bool, Error> {
+    println!("Child1 check executed!");
+    Ok(true)
+}
+async fn parent_check(_ctx: Context<'_>) -> Result<bool, Error> {
+    println!("Parent check executed!");
+    Ok(true)
+}
+
+#[poise::command(slash_command, prefix_command, check = "child2_check")]
 async fn child2(ctx: Context<'_>, _b: bool, _s: String, _i: u32) -> Result<(), Error> {
     ctx.say(ctx.invocation_string()).await?;
     Ok(())
 }
-#[poise::command(slash_command, prefix_command, subcommands("child2"))]
+#[poise::command(
+    slash_command,
+    prefix_command,
+    subcommands("child2"),
+    check = "child1_check"
+)]
 async fn child1(_ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
-#[poise::command(slash_command, prefix_command, subcommands("child1"))]
+#[poise::command(
+    slash_command,
+    prefix_command,
+    subcommands("child1"),
+    check = "parent_check"
+)]
 async fn parent(_ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
