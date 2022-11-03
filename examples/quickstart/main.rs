6ab42b06506c734fad663a87c80d92e797b8e6ed
kangalioo <jannik.a.schaper@web.de>
2022-11-03 21:01:11 +0100

    Rearrange quickstart type definitions
    To save one line and to make the ordering more logical (Data and Error always appear in this order in poise, and Context should follow after because it refers to the previous two)

--- examples/quickstart/main.rs
+++ examples/quickstart/main.rs
@@ -1,9 +1,8 @@
 use poise::serenity_prelude as serenity;
 
+struct Data {} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
-// User data, which is stored and accessible in all command invocations
-struct Data {}
 
 /// Displays your or another user's account creation date
 #[poise::command(slash_command, prefix_command)]
