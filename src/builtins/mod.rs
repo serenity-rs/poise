e9969a7060a8957a827d0b27c4d88ce0c2f50997
Andre Julius <noromoron@gmail.com>
2022-12-15 16:45:50 +0100

    Make builtins::servers aware of teams.

--- src/builtins/mod.rs
+++ src/builtins/mod.rs
@@ -191,14 +191,7 @@ pub async fn autocomplete_command<'a, U, E>(
 pub async fn servers<U, E>(ctx: crate::Context<'_, U, E>) -> Result<(), serenity::Error> {
     use std::fmt::Write as _;
 
-    let mut show_private_guilds = false;
-    if let crate::Context::Application(_) = ctx {
-        if let Ok(app) = ctx.sc().http.get_current_application_info().await {
-            if app.owner.id == ctx.author().id {
-                show_private_guilds = true;
-            }
-        }
-    }
+    let show_private_guilds = ctx.framework().options().owners.contains(&ctx.author().id);
 
     /// Stores details of a guild for the purposes of listing it in the bot guild list
     struct Guild {
