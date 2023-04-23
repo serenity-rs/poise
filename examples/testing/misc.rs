f408cbfb8c1f1e7a403212b4296d7dcd371eb6be
kangalioo <jannik.a.schaper@web.de>
2023-04-23 19:32:55 +0200

    Remove redundant import in testing crate

--- examples/testing/misc.rs
+++ examples/testing/misc.rs
@@ -1,5 +1,4 @@
 use crate::{Context, Error};
-use poise::serenity_prelude as serenity;
 
 #[poise::command(slash_command, prefix_command)]
 pub async fn paginate(ctx: Context<'_>) -> Result<(), Error> {
