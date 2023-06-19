3fc95e21e8d3ba1f146064c4f6316195bbe10444
G0ldenSp00n <hayfithyan@gmail.com>
2023-06-19 10:24:35 -0700

    feat: added support for responding to an mci with a modal

--- src/modal.rs
+++ src/modal.rs
@@ -1,5 +1,7 @@
 //! Modal trait and utility items for implementing it (mainly for the derive macro)
 
+use std::sync::Arc;
+
 use crate::serenity_prelude as serenity;
 
 /// Meant for use in derived [`Modal::parse`] implementation
@@ -90,6 +92,59 @@ pub async fn execute_modal<U: Send + Sync, E, M: Modal>(
     ))
 }
 
+/// Convenience function for showing the modal on a message interaction and waiting for a response.
+///
+/// If the user doesn't submit before the timeout expires, `None` is returned.
+///
+/// This function:
+/// 1. sends the modal via [`Modal::create()`] as a mci interaction response
+/// 2. waits for the user to submit via [`serenity::CollectModalInteraction`]
+/// 3. acknowledges the submitted data so that Discord closes the pop-up for the user
+/// 4. parses the submitted data via [`Modal::parse()`], wrapping errors in [`serenity::Error::Other`]
+///
+/// If you need more specialized behavior, you can copy paste the implementation of this function
+/// and adjust to your needs. The code of this function is just a starting point.
+pub async fn execute_component_interaction<U: Send + Sync, E, M: Modal>(
+    ctx: crate::ApplicationContext<'_, U, E>,
+    mci: Arc<serenity::MessageComponentInteraction>,
+    serenity_context: &serenity::Context,
+    defaults: Option<M>,
+    timeout: Option<std::time::Duration>,
+) -> Result<Option<M>, serenity::Error> {
+    let interaction = ctx.interaction.unwrap();
+    let interaction_id = interaction.id.to_string();
+
+    // Send modal
+    mci.create_interaction_response(serenity_context, |b| {
+        *b = M::create(defaults, interaction_id.clone());
+        b
+    })
+    .await?;
+    ctx.has_sent_initial_response
+        .store(true, std::sync::atomic::Ordering::SeqCst);
+
+    // Wait for user to submit
+    let response = serenity::CollectModalInteraction::new(&ctx.serenity_context.shard)
+        .filter(move |d| d.data.custom_id == interaction_id)
+        .timeout(timeout.unwrap_or(std::time::Duration::from_secs(3600)))
+        .await;
+    let response = match response {
+        Some(x) => x,
+        None => return Ok(None),
+    };
+
+    // Send acknowledgement so that the pop-up is closed
+    response
+        .create_interaction_response(ctx.serenity_context, |b| {
+            b.kind(serenity::InteractionResponseType::DeferredUpdateMessage)
+        })
+        .await?;
+
+    Ok(Some(
+        M::parse(response.data.clone()).map_err(serenity::Error::Other)?,
+    ))
+}
+
 /// Derivable trait for modal interactions, Discords version of interactive forms
 ///
 /// You don't need to implement this trait manually; use `#[derive(poise::Modal)]` instead
@@ -147,6 +202,15 @@ pub trait Modal: Sized {
         ctx: crate::ApplicationContext<'_, U, E>,
     ) -> Result<Option<Self>, serenity::Error> {
         execute_modal(ctx, None::<Self>, None).await
+    }
+
+    /// Calls `execute_component_interaction(ctx, mic, serenity_context, None, None)`. See [`execute_modal`]
+    async fn execute_component_interaction<U: Send + Sync, E>(
+        ctx: crate::ApplicationContext<'_, U, E>,
+        mci: Arc<serenity::MessageComponentInteraction>,
+        serenity_context: &serenity::Context,
+    ) -> Result<Option<Self>, serenity::Error> {
+        execute_component_interaction(ctx, mci, serenity_context, None::<Self>, None).await
     }
 
     /// Calls `execute_modal(ctx, Some(defaults), None)`. See [`execute_modal`]
