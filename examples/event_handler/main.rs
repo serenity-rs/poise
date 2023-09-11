f9c91ccad90faa448da079af1da0c902bae8f29c
Gabriel <69007475+Friendly-Banana@users.noreply.github.com>
2023-09-11 19:46:52 +0200

    Add a simple example for the event handler (#199)
    Fixes #188

---
+++ examples/event_handler/main.rs
@@ -0,0 +1,70 @@
+use std::env::var;
+use std::sync::atomic::{AtomicU32, Ordering};
+
+use poise::serenity_prelude as serenity;
+use poise::Event;
+
+// Types used by all command functions
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+// Custom user data passed to all command functions
+pub struct Data {
+    poise_mentions: AtomicU32,
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let options = poise::FrameworkOptions {
+        event_handler: |_ctx, event, _framework, _data| {
+            Box::pin(event_handler(_ctx, event, _framework, _data))
+        },
+        ..Default::default()
+    };
+
+    poise::Framework::builder()
+        .token(
+            var("DISCORD_TOKEN")
+                .expect("Missing `DISCORD_TOKEN` env var, see README for more information."),
+        )
+        .setup(move |_ctx, _ready, _framework| {
+            Box::pin(async move {
+                Ok(Data {
+                    poise_mentions: AtomicU32::new(0),
+                })
+            })
+        })
+        .options(options)
+        .intents(
+            serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT,
+        )
+        .run()
+        .await
+        .unwrap();
+}
+
+async fn event_handler(
+    ctx: &serenity::Context,
+    event: &Event<'_>,
+    _framework: poise::FrameworkContext<'_, Data, Error>,
+    data: &Data,
+) -> Result<(), Error> {
+    match event {
+        Event::Ready { data_about_bot } => {
+            println!("Logged in as {}", data_about_bot.user.name);
+        }
+        Event::Message { new_message } => {
+            if new_message.content.to_lowercase().contains("poise") {
+                let mentions = data.poise_mentions.load(Ordering::SeqCst) + 1;
+                data.poise_mentions.store(mentions, Ordering::SeqCst);
+                new_message
+                    .reply(ctx, format!("Poise has been mentioned {} times", mentions))
+                    .await?;
+            }
+        }
+        _ => {}
+    }
+    Ok(())
+}
