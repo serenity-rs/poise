6aaa9c5d72aff77545b2ad88b78c644dcc453991
GnomedDev <david2005thomas@gmail.com>
2024-01-11 01:19:13 +0000

    Fix breakage caused by AsRef removal and error cleanup

--- examples/feature_showcase/main.rs
+++ examples/feature_showcase/main.rs
@@ -119,7 +119,7 @@ async fn main() {
     let intents =
         serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
 
-    let client = serenity::ClientBuilder::new(token, intents)
+    let client = serenity::ClientBuilder::new(&token, intents)
         .framework(framework)
         .await;
 
