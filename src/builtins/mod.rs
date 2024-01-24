e5e5736d8c54e00b3ec5ec971a3444fe19b46454
GnomedDev <david2005thomas@gmail.com>
2024-01-24 22:13:09 +0000

    Fix doc test and clippy warning

--- src/builtins/mod.rs
+++ src/builtins/mod.rs
@@ -11,7 +11,7 @@ mod paginate;
 #[cfg(feature = "chrono")]
 pub use paginate::*;
 
-use crate::{serenity::CreateAllowedMentions, serenity_prelude as serenity, CreateReply};
+use crate::{serenity_prelude as serenity, serenity_prelude::CreateAllowedMentions, CreateReply};
 
 /// An error handler that logs errors either via the [`tracing`] crate or via a Discord message. Set
 /// up a logger like tracing subscriber
