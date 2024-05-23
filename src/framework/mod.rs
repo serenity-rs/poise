198c62d83a30811a4fa4801a23d2672edeb40caf
GnomedDev <david2005thomas@gmail.com>
2024-05-23 15:37:46 +0100

    Fix breakage caused by TeamMember::permissions removal

--- src/framework/mod.rs
+++ src/framework/mod.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 
 pub use builder::*;
 
-use crate::serenity_prelude as serenity;
+use crate::serenity_prelude::{self as serenity, TeamMemberRole};
 
 mod builder;
 
@@ -184,9 +184,12 @@ pub async fn insert_owners_from_http(
 
     if let Some(team) = application_info.team {
         for member in team.members {
-            // This `if` currently always evaluates to true but it becomes important once
-            // Discord implements more team roles than Admin
-            if member.permissions.iter().any(|p| p == "*") {
+            // "Owner" is considered anyone with permission to access the token.
+            if member.membership_state != serenity::MembershipState::Accepted {
+                continue;
+            }
+
+            if let TeamMemberRole::Admin | TeamMemberRole::Developer = member.role {
                 owners.insert(member.user.id);
             }
         }
