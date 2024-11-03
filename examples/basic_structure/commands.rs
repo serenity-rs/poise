0ce00ccb1ef9800f7ffe38ad8fb2c6dc8f9c791b
GnomedDev <david2005thomas@gmail.com>
2024-11-03 00:41:19 +0000

    Remove help commands from built-ins

--- examples/basic_structure/commands.rs
+++ examples/basic_structure/commands.rs
@@ -1,25 +1,5 @@
 use crate::{Context, Error};
 
-/// Show this help menu
-#[poise::command(prefix_command, track_edits, slash_command)]
-pub async fn help(
-    ctx: Context<'_>,
-    #[description = "Specific command to show help about"]
-    #[autocomplete = "poise::builtins::autocomplete_command"]
-    command: Option<String>,
-) -> Result<(), Error> {
-    poise::builtins::help(
-        ctx,
-        command.as_deref(),
-        poise::builtins::HelpConfiguration {
-            extra_text_at_bottom: "This is an example bot made to showcase features of my custom Discord bot framework",
-            ..Default::default()
-        },
-    )
-    .await?;
-    Ok(())
-}
-
 /// Vote for something
 ///
 /// Enter `~vote pumpkin` to vote for pumpkins
