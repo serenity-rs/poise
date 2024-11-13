d80e5c6de87672d586e4bd9fab02c20e039b9fa5
Gnome! <david2005thomas@Gmail.com>
2024-11-13 21:11:25 +0000

    Remove help commands from built-ins (#323)
    * Remove help commands from built-ins
    
    * Fix up docs and remove help_text_fn

--- examples/feature_showcase/builtins.rs
+++ examples/feature_showcase/builtins.rs
@@ -6,23 +6,3 @@ pub async fn servers(ctx: Context<'_>) -> Result<(), Error> {
     poise::builtins::servers(ctx).await?;
     Ok(())
 }
-
-#[poise::command(slash_command, prefix_command)]
-pub async fn help(ctx: Context<'_>, command: Option<String>) -> Result<(), Error> {
-    let configuration = poise::builtins::HelpConfiguration {
-        // [configure aspects about the help message here]
-        ..Default::default()
-    };
-    poise::builtins::help(ctx, command.as_deref(), configuration).await?;
-    Ok(())
-}
-
-#[poise::command(slash_command, prefix_command)]
-pub async fn pretty_help(ctx: Context<'_>, command: Option<String>) -> Result<(), Error> {
-    let configuration = poise::builtins::PrettyHelpConfiguration {
-        // [configure aspects about the help message here]
-        ..Default::default()
-    };
-    poise::builtins::pretty_help(ctx, command.as_deref(), configuration).await?;
-    Ok(())
-}
