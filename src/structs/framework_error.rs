4b8ba9220ad4e8609993cd080472525a0620509d
GnomedDev <daisy2005thomas@gmail.com>
2025-02-22 15:28:33 +0000

    Remove unnecessary event_handler field

--- src/structs/framework_error.rs
+++ src/structs/framework_error.rs
@@ -9,17 +9,6 @@ use crate::serenity_prelude as serenity;
 #[derive(derivative::Derivative)]
 #[derivative(Debug)]
 pub enum FrameworkError<'a, U, E> {
-    /// User code threw an error in generic event event handler
-    #[non_exhaustive]
-    EventHandler {
-        /// Error which was thrown in the event handler code
-        error: E,
-        /// Which event was being processed when the error occurred
-        event: &'a serenity::FullEvent,
-        /// The Framework passed to the event
-        #[derivative(Debug = "ignore")]
-        framework: crate::FrameworkContext<'a, U, E>,
-    },
     /// Error occurred during command execution
     #[non_exhaustive]
     Command {
@@ -201,7 +190,6 @@ impl<'a, U: Send + Sync + 'static, E> FrameworkError<'a, U, E> {
     /// Returns the [`serenity::Context`] of this error
     pub fn serenity_context(&self) -> &'a serenity::Context {
         match *self {
-            Self::EventHandler { framework, .. } => framework.serenity_context,
             Self::Command { ctx, .. } => ctx.serenity_context(),
             Self::SubcommandRequired { ctx } => ctx.serenity_context(),
             Self::CommandPanic { ctx, .. } => ctx.serenity_context(),
@@ -241,8 +229,7 @@ impl<'a, U: Send + Sync + 'static, E> FrameworkError<'a, U, E> {
             Self::DmOnly { ctx, .. } => ctx,
             Self::NsfwOnly { ctx, .. } => ctx,
             Self::CommandCheckFailed { ctx, .. } => ctx,
-            Self::EventHandler { .. }
-            | Self::UnknownCommand { .. }
+            Self::UnknownCommand { .. }
             | Self::UnknownInteraction { .. }
             | Self::NonCommandMessage { .. }
             | Self::DynamicPrefix { .. } => return None,
@@ -295,11 +282,6 @@ impl<U: Send + Sync + 'static, E: std::fmt::Display> std::fmt::Display
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::EventHandler { event, .. } => write!(
-                f,
-                "error in {} event event handler",
-                event.snake_case_name()
-            ),
             Self::Command { error: _, ctx } => {
                 write!(f, "error in command `{}`", full_command_name!(ctx))
             }
@@ -422,7 +404,6 @@ where
 {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
-            Self::EventHandler { error, .. } => Some(error),
             Self::Command { error, .. } => Some(error),
             Self::SubcommandRequired { .. } => None,
             Self::CommandPanic { .. } => None,
