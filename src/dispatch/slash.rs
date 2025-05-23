bfb710a18e97facb2808c7ec546bc3d2a38f10e3
TapGhoul <git@tapg.dev>
2025-05-23 22:53:04 +1000

    Associate PartialMember information with provided User within context menu application commands

--- src/dispatch/slash.rs
+++ src/dispatch/slash.rs
@@ -133,8 +133,12 @@ async fn run_command<U, E>(
             ) {
                 (
                     Some(crate::ContextMenuCommandAction::User(action)),
-                    Some(serenity::ResolvedTarget::User(user, _)),
-                ) => action(ctx, (*user).clone()).await,
+                    Some(serenity::ResolvedTarget::User(user, member)),
+                ) => {
+                    let mut user = (*user).clone();
+                    user.member = member.map(|v| Box::new(v.clone()));
+                    action(ctx, user).await
+                },
                 _ => return Err(command_structure_mismatch_error),
             }
         }
