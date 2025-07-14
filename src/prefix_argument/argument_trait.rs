7f265e554a9a90068ea4df5dc502f38255bc0d59
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2025-07-14 15:58:58 -0400

    Remove specialization hacks for `PopArgument` and `SlashArgument` (#357)
    Replaces the specialization of blanket impls with macro-generated impls for
    types that explicitly implement `serenity::ArgumentConvert`, and introduce a
    new `#[string]` attribute to signal that a type's `FromStr` implementation
    should be used.
    
    Also replaces hacky blanket impls with macro-generated impls for specific
    types, including some types that did not have impls before, to line up with the
    list of types that implement `PopArgument`.

--- src/prefix_argument/argument_trait.rs
+++ src/prefix_argument/argument_trait.rs
@@ -1,106 +1,43 @@
-//! Trait implemented for all types usable as prefix command parameters. This file also includes
-//! the auto-deref specialization emulation code to e.g. support more strings for bool parameters
-//! instead of the `FromStr` ones
+//! Trait implemented for all types usable as prefix command parameters.
+//!
+//! Many of these implementations defer to [`serenity::ArgumentConvert`].
 
 use super::{pop_string, InvalidBool, MissingAttachment, TooFewArguments};
 use crate::serenity_prelude as serenity;
-use std::marker::PhantomData;
 
-/// Full version of [`crate::PopArgument::pop_from`].
-///
-/// Uses specialization to get full coverage of types. Pass the type as the first argument
-#[macro_export]
-macro_rules! pop_prefix_argument {
-    ($target:ty, $args:expr, $attachment_id:expr, $ctx:expr, $msg:expr) => {{
-        use $crate::PopArgumentHack as _;
-        (&std::marker::PhantomData::<$target>).pop_from($args, $attachment_id, $ctx, $msg)
-    }};
-}
+/// The result of [`PopArgument::pop_from`].
+///  - If Ok, this is `(remaining, attachment_index, T)`
+///  - If Err, this is `(error, failing_arg)`
+pub(crate) type PopArgumentResult<'a, T> =
+    Result<(&'a str, usize, T), (Box<dyn std::error::Error + Send + Sync>, Option<String>)>;
 
 /// Parse a value out of a string by popping off the front of the string. Discord message context
 /// is available for parsing, and IO may be done as part of the parsing.
 ///
 /// Implementors should assume that a string never starts with whitespace, and fail to parse if it
-/// does. This is for consistency's
-/// sake and also because it keeps open the possibility of parsing whitespace.
+/// does. This is for consistency's sake and also because it keeps open the possibility of parsing
+/// whitespace.
 ///
 /// Similar in spirit to [`std::str::FromStr`].
 #[async_trait::async_trait]
 pub trait PopArgument<'a>: Sized {
-    /// Parse [`Self`] from the front of the given string and return a tuple of the remaining string
-    /// and [`Self`]. If parsing failed, an error is returned and, if applicable, the string on
-    /// which parsing failed.
-    ///
-    /// If parsing fails because the string is empty, use the `TooFewArguments` type as the error.
-    ///
-    /// Don't call this method directly! Use [`crate::pop_prefix_argument!`]
+    /// Pops an argument from the `args` string, and parses it as `Self`.
     async fn pop_from(
         args: &'a str,
         attachment_index: usize,
         ctx: &serenity::Context,
         msg: &serenity::Message,
-    ) -> Result<(&'a str, usize, Self), (Box<dyn std::error::Error + Send + Sync>, Option<String>)>;
-}
-
-#[doc(hidden)]
-#[async_trait::async_trait]
-pub trait PopArgumentHack<'a, T>: Sized {
-    async fn pop_from(
-        self,
-        args: &'a str,
-        attachment_index: usize,
-        ctx: &serenity::Context,
-        msg: &serenity::Message,
-    ) -> Result<(&'a str, usize, T), (Box<dyn std::error::Error + Send + Sync>, Option<String>)>;
+    ) -> PopArgumentResult<'a, Self>;
 }
 
 #[async_trait::async_trait]
-impl<'a, T: serenity::ArgumentConvert + Send> PopArgumentHack<'a, T> for PhantomData<T>
-where
-    T::Err: std::error::Error + Send + Sync + 'static,
-{
+impl<'a> PopArgument<'a> for bool {
     async fn pop_from(
-        self,
         args: &'a str,
         attachment_index: usize,
         ctx: &serenity::Context,
         msg: &serenity::Message,
-    ) -> Result<(&'a str, usize, T), (Box<dyn std::error::Error + Send + Sync>, Option<String>)>
-    {
-        let (args, string) =
-            pop_string(args).map_err(|_| (TooFewArguments::default().into(), None))?;
-        let object = T::convert(ctx, msg.guild_id, Some(msg.channel_id), &string)
-            .await
-            .map_err(|e| (e.into(), Some(string)))?;
-
-        Ok((args.trim_start(), attachment_index, object))
-    }
-}
-
-#[async_trait::async_trait]
-impl<'a, T: PopArgument<'a> + Send + Sync> PopArgumentHack<'a, T> for &PhantomData<T> {
-    async fn pop_from(
-        self,
-        args: &'a str,
-        attachment_index: usize,
-        ctx: &serenity::Context,
-        msg: &serenity::Message,
-    ) -> Result<(&'a str, usize, T), (Box<dyn std::error::Error + Send + Sync>, Option<String>)>
-    {
-        T::pop_from(args, attachment_index, ctx, msg).await
-    }
-}
-
-#[async_trait::async_trait]
-impl<'a> PopArgumentHack<'a, bool> for &PhantomData<bool> {
-    async fn pop_from(
-        self,
-        args: &'a str,
-        attachment_index: usize,
-        ctx: &serenity::Context,
-        msg: &serenity::Message,
-    ) -> Result<(&'a str, usize, bool), (Box<dyn std::error::Error + Send + Sync>, Option<String>)>
-    {
+    ) -> PopArgumentResult<'a, Self> {
         let (args, string) =
             pop_string(args).map_err(|_| (TooFewArguments::default().into(), None))?;
 
@@ -115,17 +52,13 @@ impl<'a> PopArgumentHack<'a, bool> for &PhantomData<bool> {
 }
 
 #[async_trait::async_trait]
-impl<'a> PopArgumentHack<'a, serenity::Attachment> for &PhantomData<serenity::Attachment> {
+impl<'a> PopArgument<'a> for serenity::Attachment {
     async fn pop_from(
-        self,
         args: &'a str,
         attachment_index: usize,
         ctx: &serenity::Context,
         msg: &serenity::Message,
-    ) -> Result<
-        (&'a str, usize, serenity::Attachment),
-        (Box<dyn std::error::Error + Send + Sync>, Option<String>),
-    > {
+    ) -> PopArgumentResult<'a, Self> {
         let attachment = msg
             .attachments
             .get(attachment_index)
@@ -134,6 +67,83 @@ impl<'a> PopArgumentHack<'a, serenity::Attachment> for &PhantomData<serenity::At
 
         Ok((args, attachment_index + 1, attachment))
     }
+}
+
+#[async_trait::async_trait]
+impl<'a> PopArgument<'a> for String {
+    async fn pop_from(
+        args: &'a str,
+        attachment_index: usize,
+        ctx: &serenity::Context,
+        msg: &serenity::Message,
+    ) -> PopArgumentResult<'a, Self> {
+        match pop_string(args) {
+            Ok((args, string)) => Ok((args, attachment_index, string)),
+            Err(err) => Err((err.into(), Some(args.into()))),
+        }
+    }
+}
+
+/// Pops a string and then converts it to `T` using its `ArgumentConvert` implementation.
+async fn pop_from_argumentconvert<'a, T>(
+    args: &'a str,
+    attachment_index: usize,
+    ctx: &serenity::Context,
+    msg: &serenity::Message,
+) -> PopArgumentResult<'a, T>
+where
+    T: serenity::ArgumentConvert + Send,
+    T::Err: std::error::Error + Send + Sync + 'static,
+{
+    let (args, string) = pop_string(args).map_err(|_| (TooFewArguments::default().into(), None))?;
+    let object = T::convert(ctx, msg.guild_id, Some(msg.channel_id), &string)
+        .await
+        .map_err(|e| (e.into(), Some(string)))?;
+
+    Ok((args.trim_start(), attachment_index, object))
+}
+
+/// Auto-impls `PopArgument` for a type by deferring to [`pop_from_argumentconvert`].
+macro_rules! argumentconvert_pop_argument {
+    ( $(
+        $( #[cfg(feature = $feature:literal)] )?
+        $type:ty,
+    )* ) => {
+        $(
+            $( #[cfg(feature = $feature)] )?
+            #[async_trait::async_trait]
+            impl<'a> PopArgument<'a> for $type {
+                async fn pop_from(
+                    args: &'a str,
+                    attachment_index: usize,
+                    ctx: &serenity::Context,
+                    msg: &serenity::Message,
+                ) -> PopArgumentResult<'a, Self> {
+                    pop_from_argumentconvert(args, attachment_index, ctx, msg).await
+                }
+            }
+        )*
+    }
+}
+
+argumentconvert_pop_argument! {
+    // Via blanket impl of `ArgumentConvert` for `T: FromStr`
+    f32, f64,
+    u8, u16, u32, u64,
+    i8, i16, i32, i64,
+    serenity::Mention,
+
+    // Via explicit `ArgumentConvert` impls
+    serenity::User, serenity::Member,
+    serenity::Message,
+    serenity::Channel, serenity::GuildChannel,
+    serenity::EmojiId, serenity::Emoji,
+    serenity::Role,
+
+    #[cfg(feature = "cache")]
+    serenity::GuildId,
+    #[cfg(feature = "cache")]
+    serenity::Guild,
 }
 
 /// Macro to allow for using mentions in snowflake types
@@ -158,17 +168,13 @@ macro_rules! snowflake_pop_argument {
         }
 
         #[async_trait::async_trait]
-        impl<'a> PopArgumentHack<'a, $type> for &PhantomData<$type> {
+        impl<'a> PopArgument<'a> for $type {
             async fn pop_from(
-                self,
                 args: &'a str,
                 attachment_index: usize,
                 ctx: &serenity::Context,
                 msg: &serenity::Message,
-            ) -> Result<
-                (&'a str, usize, $type),
-                (Box<dyn std::error::Error + Send + Sync>, Option<String>),
-            > {
+            ) -> PopArgumentResult<'a, Self> {
                 let (args, string) =
                     pop_string(args).map_err(|_| (TooFewArguments::default().into(), None))?;
 
