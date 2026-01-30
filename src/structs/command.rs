e850875cf3925786502d9f126c85f8c61f89ace2
zkxs <zkxs00@gmail.com>
2026-01-30 16:17:27 -0600

    Fix `default_member_permissions` not being checked for `#[command(context_menu_command)]` (#297)

--- src/structs/command.rs
+++ src/structs/command.rs
@@ -248,6 +248,12 @@ impl<U, E> Command<U, E> {
             crate::ContextMenuCommandAction::__NonExhaustive => unreachable!(),
         });
 
+        // This is_empty check is needed because Discord special cases empty
+        // default_member_permissions to mean "admin-only"
+        if !self.default_member_permissions.is_empty() {
+            builder = builder.default_member_permissions(self.default_member_permissions);
+        }
+
         if self.guild_only {
             builder = builder.contexts(vec![serenity::InteractionContext::Guild]);
         } else if self.dm_only {
