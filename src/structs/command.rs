7b6dd00cf0b72667a614e9775b65b1f1c4d584b9
Rose Davidson <rose@metaclassical.com>
2026-01-31 10:26:58 -0800

    Check interaction type in command matching (#381)
    This prevents errors from trying to invoke a message interaction on a user command or vice versa.
    
    Fixes #380

--- src/structs/command.rs
+++ src/structs/command.rs
@@ -250,11 +250,7 @@ impl<U, E> Command<U, E> {
 
         // TODO: localization?
         let name = self.context_menu_name.as_deref().unwrap_or(&self.name);
-        let mut builder = serenity::CreateCommand::new(name).kind(match context_menu_action {
-            crate::ContextMenuCommandAction::User(_) => serenity::CommandType::User,
-            crate::ContextMenuCommandAction::Message(_) => serenity::CommandType::Message,
-            crate::ContextMenuCommandAction::__NonExhaustive => unreachable!(),
-        });
+        let mut builder = serenity::CreateCommand::new(name).kind(context_menu_action.into());
 
         // This is_empty check is needed because Discord special cases empty
         // default_member_permissions to mean "admin-only"
